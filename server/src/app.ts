20ded791a8d7e5ac3d86aef6a80726c8f456c501
Jackson <jacksonrcohen@gmail.com>
2022-01-30 16:27:13 -0500

    Add basic socket-io configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,28 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
+import createError from 'http-errors';
+import socketServer from './socket';
+import socket from './socket';
+import 'reflect-metadata';
 
 const app: Application = express();
-const port: number = 3001;
 
-app.get('/toto', (req: Request, res: Response) => {
-  res.send('Hello goto');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
 });
 
-app.listen(port, function () {
-  console.log(`App is listening on port ${port} !`);
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+export default app;
---
+++ server/src/socket.ts
@@ -0,0 +1,18 @@
+import { Server } from 'socket.io';
+import { useSocketServer } from 'socket-controllers';
+
+export default (httpServer) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  io.on('connection', (socket) => {});
+
+  useSocketServer(io, {
+    controllers: [__dirname + '/api/controllers/*.ts'],
+  });
+
+  return io;
+};
