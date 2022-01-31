5d0af04af2bc09e3bfb3c8d44b0e874e653fb3ab
Jackson <jacksonrcohen@gmail.com>
2022-01-30 19:28:20 -0500

    Add class based SocketService

---
+++ client/src/services/socketService/index.ts
@@ -0,0 +1,24 @@
+import { io, Socket } from 'socket.io-client';
+
+class SocketService {
+  public socket: Socket | null = null;
+
+  public connect(url: string): Promise<Socket<any, any>> {
+    return new Promise((resolve, reject) => {
+      this.socket = io(url);
+
+      if (!this.socket) return reject();
+
+      this.socket.on('connect', () => {
+        resolve(this.socket as Socket);
+      });
+
+      this.socket.on('connect_error', (error) => {
+        console.log('Connection error: ', error);
+        reject(error);
+      });
+    });
+  }
+}
+
+export default new SocketService();
