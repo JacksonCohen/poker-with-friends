63b62b942ea71853be559ce71d905456ad8b10d4
Jackson <jacksonrcohen@gmail.com>
2022-01-30 19:48:32 -0500

    Connect sockets to JoinRoom component

--- client/src/components/JoinRoom/index.tsx
+++ client/src/components/JoinRoom/index.tsx
@@ -1,26 +1,49 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
+import gameContext from '../../contexts/gameContext';
+import gameService from '../../services/gameService';
+import socketService from '../../services/socketService';
 
 function JoinRoom() {
   const [roomName, setRoomName] = useState('');
+  const [isJoining, setIsJoining] = useState(false);
+  const { isInRoom, setIsInRoom } = useContext(gameContext);
 
   const handleRoomNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setRoomName(event?.target.value);
   };
 
-  const handleSubmit = () => {};
+  const joinRoom = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const socket = socketService.socket;
+    if (!socket || !roomName || roomName.trim() === '') return;
+    setIsJoining(true);
+
+    const joined = await gameService.joinGameRoom(socket, roomName).catch((error) => {
+      alert(error);
+    });
+
+    if (joined) setIsInRoom(true);
+    setIsJoining(false);
+  };
 
   return (
     <div className='w-full h-full flex flex-col items-center justify-center mt-8'>
       <h3 className='text-white'>Enter room ID to join a table</h3>
-      <form action='submit'>
+      <form action='submit' onSubmit={joinRoom} className='flex flex-col items-center'>
         <input
           type='text'
           className='w-80 h-8 bg-white outline-none border-2 border-solid border-red-200 rounded py-0 px-3'
           placeholder='Room ID'
+          value={roomName}
           onChange={handleRoomNameChange}
         />
-        <button className='outline-none bg-red-300 text-white text-lg border-2 border-solid border-transparent rounded mt-4 cursor-pointer hover:bg-transparent hover:border-green-200'>
-          Join
+        <button
+          type='submit'
+          className='w-16 outline-none bg-red-300 text-white text-lg border-2 border-solid border-transparent rounded mt-4 cursor-pointer hover:bg-transparent hover:border-green-200'
+          disabled={isJoining}
+        >
+          {isJoining ? 'Joining...' : 'Join'}
         </button>
       </form>
     </div>
