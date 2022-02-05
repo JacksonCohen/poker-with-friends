1250d033220a6942f9b855c5dffc81f45e8c3838
Jackson <jacksonrcohen@gmail.com>
2022-02-04 19:06:02 -0500

    Center join prompt

--- client/src/components/JoinRoom/index.tsx
+++ client/src/components/JoinRoom/index.tsx
@@ -29,23 +29,25 @@ function JoinRoom() {
 
   return (
     <div className='w-full h-full flex flex-col items-center justify-center mt-8'>
-      <h3 className='text-white mb-2'>Enter room ID to join a table</h3>
-      <form action='submit' onSubmit={joinRoom} className='flex flex-col items-center'>
-        <input
-          type='text'
-          className='w-80 h-8 bg-white outline-none border-2 border-solid border-red-200 rounded py-0 px-3 mb-4'
-          placeholder='Room ID'
-          value={roomName}
-          onChange={handleRoomNameChange}
-        />
-        <button
-          type='submit'
-          className='w-24 outline-none bg-red-300 text-white text-lg border-2 border-solid border-transparent rounded cursor-pointer hover:bg-transparent hover:border-green-200'
-          disabled={isJoining}
-        >
-          {isJoining ? 'Joining...' : 'Join'}
-        </button>
-      </form>
+      <div className='text-center -translate-y-1/2'>
+        <h3 className='text-white mb-2'>Enter room ID to join a table</h3>
+        <form action='submit' onSubmit={joinRoom} className='flex flex-col items-center'>
+          <input
+            type='text'
+            className='w-80 h-8 bg-white outline-none border-2 border-solid border-red-200 rounded py-0 px-3 mb-4'
+            placeholder='Room ID'
+            value={roomName}
+            onChange={handleRoomNameChange}
+          />
+          <button
+            type='submit'
+            className='w-24 outline-none bg-red-300 text-white text-lg border-2 border-solid border-transparent rounded cursor-pointer hover:bg-transparent hover:border-green-200'
+            disabled={isJoining}
+          >
+            {isJoining ? 'Joining...' : 'Join'}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
